2acf47078507762dd7a8d51059524f1ced836136
OfficialCRUGG <ich@crugg.de>
2024-11-02 04:03:52 +0100

    chore: lib.ts

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,8 @@
-export function hello() {
-  console.log("Hello, world!");
-}
+export * from "./lib/Neko";
+export { default as markdown } from "./lib/markdown";
+export * from "./lib/pdf";
+export * from "./lib/formats";
+export * from "./lib/margins";
+
+export * from "./utils/parseMargin";
+export * from "./utils/parseFormat";
